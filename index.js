ae28ad2bb8ab05f38642baa2611a8d7c3f631724
shahzadahmadbcs <iamahmad321@gmail.com>
2023-11-16 00:20:10 +0500

    day 1 js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const a = 20 ;
+// array length
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let size = fruits.length;
+// pop
+const car = ["mg", "toyota", "bmw", "corolla"];
+fruits.pop();
+// push
+const phones=["apple","samsung","lg","huauvi"]
+phones.push("infinix");
+// shift
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+// unshift
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon");
